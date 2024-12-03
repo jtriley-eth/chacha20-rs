caecf7f93d99c40623a2f0a06ed415c67b216d82
Joshua Trujillo <jtriley15@gmail.com>
2024-12-03 23:27:21 +0000

    fmt

--- benches/mod.rs
+++ benches/mod.rs
@@ -1,11 +1,10 @@
-use std::hint::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 
-use chacha20::{algo, simd, quad_simd};
+use chacha20::{algo, quad_simd, simd};
 
 const KEY: [u32; 8] = [
-    0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918,
-    0x1f1e1d1c,
+    0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c,
 ];
 
 const NONCE: [u32; 3] = [0x09000000, 0x4a000000, 0x00000000];
